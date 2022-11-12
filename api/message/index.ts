59c469bb74332a962ef36849f32d2efea794df49
Máté Bányi <banyimatex@gmail.com>
2022-11-12 22:50:26 +0100

    update

--- api/message/index.ts
+++ api/message/index.ts
@@ -22,7 +22,7 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     } catch (error) {
         context.res = {
             status: 401,
-            body: error
+            body: {error: error, authHeader: headerValue, fullHeader: req.headers}
         };
     }
 };
